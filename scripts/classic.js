cd328eeeecf53fa42d53342b62ed25297116f066
list <fernandokodagawa@gmail.com>
2026-01-07 13:36:31 -0300

    Update classic.js

--- scripts/classic.js
+++ scripts/classic.js
@@ -11,7 +11,24 @@ tile[7].addEventListener("click", () => play(7));
 tile[8].addEventListener("click", () => play(8));
 
 function play(i) {
-  let x = document.querySelector(".x");
-  x.classList.add("played");
-  console.log(x);
+  let btn = document.querySelector("#tile_" + (i + 1));
+
+  if (is_player1() == true) {
+    btn.innerHTML = `<p>O</p>`;
+  
+  } else {
+    btn.innerHTML = `<p>X</p>`;
+  }
+}
+
+let turn = 1;
+function is_player1() {
+  let is_player1 = true;
+  
+  if (turn % 2 == 0) {
+    is_player1 = false;
+  }
+  turn++;
+
+  return is_player1;
 }
